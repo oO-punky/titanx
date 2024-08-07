ca0fa94a34a2ac1eaaef9a879297675c9c0bbae7
punkyOo <115904110+oO-punky@users.noreply.github.com>
2024-08-07 14:09:35 -0700

    updating few things

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import { Router } from 'itty-router'
-
-const router = Router()
-
-const { ethers, AbiCoder } = require('ethers')
 const { getTitanX, getEventFilters, getSignatures } = require('../lib/contract')
 const { addMintStartedEvents } = require('../lib/database')
 
+const router = Router()
 
 router.get('/scrape/mintstarted', async function(request, env, ctx) {
   const { provider, contract } = await getTitanX(env)
 
   const startBlock = env.START_BLOCK
   const endBlock = await provider.getBlockNumber()
-
   console.log(`number of blocks: ${endBlock - startBlock}`)
 
   for (let blockNumber = startBlock; blockNumber <= endBlock; blockNumber += 100) {
     console.log(`Processing block ${blockNumber}`);
+
     const events = await contract.queryFilter(contract.filters.MintStarted(), blockNumber, blockNumber + 100)
+    console.log(`found: ${events.length} `)
 
     if (events.length > 0) {
       for (const event of events) {
@@ -25,10 +23,7 @@ router.get('/scrape/mintstarted', async function(request, env, ctx) {
       }
     }
   }
-
 })
-
-
 
 router.get('/', async function(request, env, ctx) {
   const { provider, contract } = await getTitanX(env)
