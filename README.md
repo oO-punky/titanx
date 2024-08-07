1cb7fd306ef33fcf4b7d7af02c0743dcb77bab34
punkyOo <115904110+oO-punky@users.noreply.github.com>
2024-08-07 01:09:58 -0700

    scraping mint started data into a table

--- README.md
+++ README.md
@@ -6,3 +6,29 @@ ETL pipeline for titanx events
 - cloudflare workers
 - D1 sqlite database
 - api with cors
+
+#### titanx
+
+https://etherscan.io/token/0xf19308f923582a6f7c465e5ce7a9dc1bec6665b1
+
+
+
+- events
+```
+'Approval',
+'ApproveBurnMints',
+'ApproveBurnStakes',
+  'CyclePayoutTriggered',
+  'ETHDistributed',
+  'GlobalDailyUpdateStats',
+  'MintClaimed',
+  'MintStarted',
+  'ProtocolFeeRecevied',
+  'RewardClaimed',
+  'StakeEnded',
+  'StakeStarted',
+  'TitanBurned',
+'Transfer'
+```
+
+
--- src/index.js
+++ src/index.js
@@ -2,4 +2,41 @@ import { Router } from 'itty-router'
 
 const router = Router()
 
-router.get('/')
+const { ethers, AbiCoder } = require('ethers')
+const { getTitanX, getEventFilters, getSignatures } = require('../lib/contract')
+const { addMintStartedEvents } = require('../lib/database')
+
+
+const abi = require('../abi/titanx.abi.json')
+
+
+router.get('/scrape/mintstarted', async function(request, env, ctx) {
+  const { provider, contract } = await getTitanX(env)
+
+  const startBlock = env.START_BLOCK
+  const endBlock = await provider.getBlockNumber()
+
+  console.log(`number of blocks: ${endBlock - startBlock}`)
+
+  for (let blockNumber = startBlock; blockNumber <= endBlock; blockNumber += 100) {
+    console.log(`Processing block ${blockNumber}`);
+    const events = await contract.queryFilter(contract.filters.MintStarted(), blockNumber, blockNumber + 100)
+
+    if (events.length > 0) {
+      for (const event of events) {
+        await addMintStartedEvents(event, env);
+      }
+    }
+  }
+
+})
+
+
+router.get('/', async function(request, env, ctx) {
+  const { provider, contract } = await getTitanX(env)
+})
+router.all('*', () => new Response('Not Found.', { status: 404 }))
+
+export default {
+  fetch: router.handle
+}
